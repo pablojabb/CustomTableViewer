30ba4b8eb7f49c9e69dd95a6cf2a611ce91685a5
Anthony Jumaya <jumayaanthony@gmail.com>
2025-02-23 20:09:36 +0800

    Refactor popup component to use useReducer for count state and enhance button UI

--- popup.tsx
+++ popup.tsx
@@ -1,14 +1,21 @@
-import { useState } from "react"
-
+import { useReducer } from "react"
+ 
+import "index.css"
+ 
 function IndexPopup() {
-  const [data, setData] = useState("")
-
+  const [count, increase] = useReducer((c) => c + 1, 0)
+ 
   return (
-   <>
-   
-   <div className="bg-green-300 h-full"></div>
-   </>
+    <button
+      onClick={() => increase()}
+      type="button"
+      className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-green-300 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      Count:
+      <span className="inline-flex items-center justify-center w-8 h-4 ml-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
+        {count}
+      </span>
+    </button>
   )
 }
-
+ 
 export default IndexPopup
