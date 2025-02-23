6c314a11397d9bc582d191c6d5cb24bcac9a5373
Anthony Jumaya <jumayaanthony@gmail.com>
2025-02-23 22:23:30 +0800

    Prevent newtab component from loading unless explicitly opened

--- src/newtab.tsx
+++ src/newtab.tsx
@@ -3,13 +3,13 @@ import { useState } from "react"
 function IndexNewtab() {
   const [data, setData] = useState("")
 
+  // Prevent component from loading unless it's opened explicitly
+  if (window.location.href !== chrome.runtime.getURL("newtab.html")) {
+    return null
+  }
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
+    <div style={{ display: "flex", flexDirection: "column", padding: 16 }}>
       <h1>
         Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
       </h1>
