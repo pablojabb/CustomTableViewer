b489fff7bbac896497c551ce7bd86d8b4136ea29
Anthony Jumaya <jumayaanthony@gmail.com>
2025-02-24 11:30:56 +0800

    Remove newtab component and update popup to open a specific tab page; add new Delta Flyer and Table View tabs

--- src/popup.tsx
+++ src/popup.tsx
@@ -5,17 +5,16 @@ import "index.css"
 function IndexPopup() {
   const [count, increase] = useReducer((c) => c + 1, 0)
 
-  const openNewTab = () => {
-    chrome.tabs.create({ url: chrome.runtime.getURL("newtab.html") })
-  }
 
   return (
     <button
-      onClick={openNewTab}
-      type="button"
-      className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-green-300 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-      Open New Tab
-    </button>
+        onClick={() => {
+          chrome.tabs.create({
+            url: "./tabs/table-view.html"
+          })
+        }}>
+        open tab page
+      </button>
   )
 }
 
---
+++ src/tabs/table-view.tsx
@@ -0,0 +1,16 @@
+function tableView() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: 16
+        }}>
+        <h2>DtableView Tab</h2>
+   
+        <p>This tab is only available on the tableView.</p>
+      </div>
+    )
+  }
+   
+  export default tableView
