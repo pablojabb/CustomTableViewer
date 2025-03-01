6c691ffab18e02407bd31e4ef26cd9a2c5b98153
Anthony Jumaya <jumayaanthony@gmail.com>
2025-03-02 03:29:42 +0800

    Refactor IndexPopup styling for improved button responsiveness and footer layout

--- src/popup.tsx
+++ src/popup.tsx
@@ -48,7 +48,7 @@ function IndexPopup() {
     <>
       <div className="w-80 h-full flex flex-col justify-center items-center bg-light-bg dark:bg-dark-bg">
         <header className="w-full flex mb-2 justify-between items-center ">
-          <h1 className="text-lg font-bold my-1 text-light-text dark:text-dark-text text-center px-2">
+          <h1 className="text-lg font-bold  my-1 text-light-text dark:text-dark-text text-center px-2">
             CTV
           </h1>
           <DarkModeToggle />
@@ -59,19 +59,19 @@ function IndexPopup() {
         </div>
         <div className="w-[80%] flex justify-between items-center">
           <button
-            className="p-3 rounded-md my-2 font-semibold bg-light-m-btn dark:bg-dark-m-btn text-light-important-text dark:text-light-important-text"
+            className="px-1.5 py-3 text-sm hover:contrast-125 transition-colors rounded-md my-2 font-semibold bg-light-m-btn dark:bg-dark-m-btn text-light-important-text dark:text-light-important-text"
             onClick={handleExtractTable}>
             Extract Table Data
           </button>
 
           <button
-            className="p-3 rounded-md my-2 font-semibold bg-light-s-btn dark:bg-dark-s-btn text-light-important-text dark:text-dark-important-text"
+            className="px-1.5 py-3 hover:brightness-125 transition-colors text-sm rounded-md my-2 font-semibold bg-light-s-btn dark:bg-dark-s-btn text-light-important-text dark:text-dark-important-text"
             onClick={handleClick}>
             Open Table Page
           </button>
         </div>
-        <footer className="w-full flex justify-center items-center mt-4 bg-gray-600">
-          <h1 className="text-sm font-sans font-medium mb-1 mt-2 text-center px-2">
+        <footer className="w-full flex justify-center items-center mt-4 mb-2">
+          <h1 className="text-xs font-sans font-medium mb-1 mt-2 text-center px-2 text-light-important-text dark:text-dark-important-text">
             Made with <span className="inline-block animate-pulse">❤️‍🔥</span> by{" "}
             <a
               className="font-semibold hover:underline underline-offset-4"
