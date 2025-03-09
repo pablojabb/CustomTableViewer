e564d15b1a3a8d9c07bba0d1a8e79f95e046b19d
Anthony Jumaya <jumayaanthony@gmail.com>
2025-03-09 14:59:58 +0800

    Refactor Table component: integrate useEvents hook for dynamic event handling and remove hardcoded events

--- src/tabs/components/Table.tsx
+++ src/tabs/components/Table.tsx
@@ -1,19 +1,25 @@
 import FullCalendar from "@fullcalendar/react"
 import timeGridPlugin from "@fullcalendar/timegrid"
+import useEvents from "../hook/useEvents"
 
 const Table = ({ tableData }) => {
   if (tableData.length === 0) {
     return (
       <div className="flex justify-center items-center mt-[40vh]">
-        <p className="text-sm sm:text-base md:text-lg lg:text-2xl text-red-500 text-center">
+        <p className="text-sm sm:text-base md:text-lg lg:text-2xl text-center">
           No table data found.
         </p>
       </div>
     )
   }
-
+ 
+  const events = useEvents(tableData);
+  // console.log(events)
+  
+  
   return (
     <>
+    
       <div className="rildiv">
         <FullCalendar
           plugins={[timeGridPlugin]}
@@ -27,50 +33,9 @@ const Table = ({ tableData }) => {
           dayHeaderFormat={{ weekday: "long" }}
           headerToolbar={false}
           eventColor="#3feb56"
-          events={[
-            {
-              title: "Team Meeting",
-              start: new Date().setHours(10, 0, 0), // Today at 10 AM
-              end: new Date().setHours(11, 0, 0)
-            },
-            {
-              title: "Project Deadline",
-              start: new Date(
-                new Date().setDate(new Date().getDate() - 1)
-              ).setHours(14, 0, 0), // Tomorrow at 2 PM
-              end: new Date(
-                new Date().setDate(new Date().getDate() - 1)
-              ).setHours(15, 0, 0)
-            },
-            {
-              title: "Client Presentation",
-              start: new Date(
-                new Date().setDate(new Date().getDate() - 2)
-              ).setHours(9, 20, 0), // Two days from now at 9:30 AM
-              end: new Date(
-                new Date().setDate(new Date().getDate() - 2)
-              ).setHours(10, 10, 0)
-            },
-            {
-              title: "Workout Session",
-              start: new Date(
-                new Date().setDate(new Date().getDate() - 3)
-              ).setHours(18, 22, 0), // Three days from now at 6 PM
-              end: new Date(
-                new Date().setDate(new Date().getDate() - 3)
-              ).setHours(19, 33, 0)
-            },
-            {
-              title: "Code Review",
-              start: new Date(
-                new Date().setDate(new Date().getDate() - 4)
-              ).setHours(17, 0, 0), // Four days from now at 4 PM
-              end: new Date(
-                new Date().setDate(new Date().getDate() - 4)
-              ).setHours(19, 30, 0)
-            }
-          ]}
+          events={events}
         />
+       
       </div>
     </>
   )
