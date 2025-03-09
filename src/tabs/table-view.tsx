25847e9ab19474ed0e2754efc00b6db892e58fa5
Anthony Jumaya <jumayaanthony@gmail.com>
2025-03-09 12:38:14 +0800

    Refactor Table component: migrate from JSX to TSX, enhance styling, and remove unnecessary padding

--- src/tabs/table-view.tsx
+++ src/tabs/table-view.tsx
@@ -12,9 +12,6 @@ const TablePage = () => {
 
   return (
     <div className="p-4">
-      <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold mb-4 text-center">
-        Extracted Table Data
-      </h1>
       <Table tableData={tableData} />
       <div className="mt-4 flex justify-center items-center">
       <button className="px-4 py-2 bg-green-500 text-white rounded" onClick={handleCloseTab}>
