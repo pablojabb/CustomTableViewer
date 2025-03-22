854dc972355c8edae19827078dfcfb79ad2122d9
Anthony Jumaya <jumayaanthony@gmail.com>
2025-03-22 18:02:38 +0800

    Refactor Card component for improved readability and consistency

--- src/tabs/components/Card.tsx
+++ src/tabs/components/Card.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
+import React from "react"
 
 const Card = ({ title, value, icon, color = "text-white" }) => {
   return (
     <div
-      title={title} 
-      className="my-4 flex flex-col justify-center items-center gap-4 border-2 border-dashed border-gray-500/50 p-4 rounded-md h-auto min-w-96 dark:text-dark-important-text"
-    >
+      title={title}
+      className="my-4 flex flex-col justify-center items-center gap-4 border-2 border-dashed border-gray-500/50 p-4 rounded-md h-auto min-w-96 dark:text-dark-important-text">
       <div className="flex gap-2 items-center justify-center">
+        {icon && <div className={`w-6 h-6 ${color}`}>{icon}</div>}
         <span className={`font-medium text-lg md:text-xl ${color}`}>
           {title}
         </span>
-        {icon && <div className={`w-6 h-6 ${color}`}>{icon}</div>}
       </div>
       <span className="text-2xl font-semibold text-center text-light-important-text dark:text-dark-important-text">
-        {value} 
+        {value}
       </span>
     </div>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
