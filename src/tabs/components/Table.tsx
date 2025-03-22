1d5c2a705e89faca440dc4476861507d95c25f16
Anthony Jumaya <jumayaanthony@gmail.com>
2025-03-22 19:05:03 +0800

    Add image download functionality to Table component

--- src/tabs/components/Table.tsx
+++ src/tabs/components/Table.tsx
@@ -1,8 +1,9 @@
 import FullCalendar from "@fullcalendar/react"
 import timeGridPlugin from "@fullcalendar/timegrid"
+import { FiArrowDownCircle } from "react-icons/fi";
 
 import DarkModeToggle from "~DarkModeToggle "
-
+import { useToJpeg } from '@hugocxl/react-to-image'
 import useScheduleEvents from "./../hook/useScheduleEvents"
 import SummaryCard from "./SummaryCard"
 
@@ -23,28 +24,39 @@ const Table = ({ tableData }) => {
 
   //TODO: Plan next update (features) 
   //TODO: Image feature
-  //TODO: Vacant Time in Summary component 
 
-  const handleDownload = async () => {
-   alert("Coming Soon😎!!")
-  }
+  // const handleDownload = async () => {
+  //   convertToPng
+  // }
+
+  const [state, convertToJpeg, ref] = useToJpeg<HTMLDivElement>({
+    onSuccess: data => {
+      const link = document.createElement('a');
+      link.href = data;
+      link.download = 'screenshot.jpg';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  });
 
   return (
     <>
-      <div className="rildiv">
-        <div className="flex justify-between items-center mb-2">
+      <div className="rildiv"   >
+        <div className="flex justify-between items-center mb-2 ">
           <DarkModeToggle />
           <button
-            className="p-2 text-sm
+            className="p-2 text-sm flex items-center justify-center
              font-semibold rounded transition-colors 
           bg-light-m-btn dark:bg-dark-m-btn 
           hover:bg-light-m-btn-hover dark:hover:bg-dark-m-btn-hover 
           active:bg-light-m-btn-active dark:active:bg-dark-m-btn-active
           text-light-content-text dark:text-dark-content-text"
-          onClick={handleDownload}>
-            Download Image
+          onClick={convertToJpeg}>
+            <span className="mr-1 text-lg"><FiArrowDownCircle/></span> Download as PNG
           </button>
         </div>
+        <div ref={ref} className="w-auto h-auto dark:bg-dark-bg bg-light-bg">
         <FullCalendar
           plugins={[timeGridPlugin]}
           initialView="timeGridWeek"
@@ -65,6 +77,8 @@ const Table = ({ tableData }) => {
             }
           }}
         />
+        </div>
+        
         <SummaryCard
           vacantDays={vacantDays}
           conflictCount={conflictCount}
